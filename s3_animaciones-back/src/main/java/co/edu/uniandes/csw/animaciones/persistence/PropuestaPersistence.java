b635c18cde579936c34f929691fbbc5d70f9a4d8
Luis José García <lj.garciam1@uniandes.edu.co>
2019-02-13 22:15:06 -0500

    Persistence (lo del lunes) funcionando

---
+++ s3_animaciones-back/src/main/java/co/edu/uniandes/csw/animaciones/persistence/PropuestaPersistence.java
@@ -0,0 +1,41 @@
+
+package co.edu.uniandes.csw.animaciones.persistence;
+
+import co.edu.uniandes.csw.animaciones.entities.PropuestaEntity;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+
+/**
+ *
+ * @author sigo siendo yo; no me he movido de aquí
+ * (García)
+ */
+@Stateless
+public class PropuestaPersistence {
+    private final static Logger  LOGGER = Logger.getLogger(PropuestaPersistence.class.getName());
+    
+    @PersistenceContext(unitName = "animacionesPU")
+    protected EntityManager em;
+    
+    public PropuestaEntity create(PropuestaEntity fE){
+        LOGGER.log(Level.INFO,"Creando una propuesta...");
+        em.persist(fE);
+        LOGGER.log(Level.INFO,"Saliendo del proceso...");
+        return fE;
+    }
+    
+    public PropuestaEntity find(Long id){
+        return em.find(PropuestaEntity.class, id);
+    }
+    
+    public List<PropuestaEntity> findAll(){
+        
+        TypedQuery<PropuestaEntity> qu = em.createQuery("select u from PropuestaEntity u", PropuestaEntity.class);
+        return qu.getResultList();
+    }
+}
