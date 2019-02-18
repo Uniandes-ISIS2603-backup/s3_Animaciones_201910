59c96f675e367452ace500a6716b3bbde060778c
Nicolas Alvarado <na.alvarado@uniandes.edu.co>
2019-02-18 11:12:47 -0500

    clase AnimacionEntity completa

--- s3_animaciones-back/src/main/java/co/edu/uniandes/csw/animaciones/entities/AnimacionEntity.java
+++ s3_animaciones-back/src/main/java/co/edu/uniandes/csw/animaciones/entities/AnimacionEntity.java
@@ -1,6 +1,7 @@
 package co.edu.uniandes.csw.animaciones.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.Date;
 import javax.persistence.CascadeType;
 import javax.persistence.Entity;
@@ -32,9 +33,89 @@ public class AnimacionEntity extends BaseEntity implements Serializable {
     @ManyToOne(cascade = CascadeType.PERSIST)
     private ArtistaEntity artista;
     
-    //@PodamExclude
-    //@OneToMany(mappedBy = "video", cascade = CascadeType.PERSIST, orphanRemoval = true)
-    // Calificaciones
+    @PodamExclude
+    @OneToMany(mappedBy = "animacion", cascade = CascadeType.PERSIST, orphanRemoval = true)
+    private ArrayList<CalificacionEntity> calificaciones = new ArrayList<>();
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(Integer precio) {
+        this.precio = precio;
+    }
+
+    public String getTecnica() {
+        return tecnica;
+    }
+
+    public void setTecnica(String tecnica) {
+        this.tecnica = tecnica;
+    }
+
+    public String getTags() {
+        return tags;
+    }
+
+    public void setTags(String tags) {
+        this.tags = tags;
+    }
+
+    public Double getCalificacion() {
+        return calificacion;
+    }
+
+    public void setCalificacion(Double calificacion) {
+        this.calificacion = calificacion;
+    }
+
+    public String getLink() {
+        return link;
+    }
+
+    public void setLink(String link) {
+        this.link = link;
+    }
+
+    public Date getFecha() {
+        return fecha;
+    }
+
+    public void setFecha(Date fecha) {
+        this.fecha = fecha;
+    }
+
+    public ArtistaEntity getArtista() {
+        return artista;
+    }
+
+    public void setArtista(ArtistaEntity artista) {
+        this.artista = artista;
+    }
+
+    public ArrayList<CalificacionEntity> getCalificaciones() {
+        return calificaciones;
+    }
+
+    public void setCalificaciones(ArrayList<CalificacionEntity> calificaciones) {
+        this.calificaciones = calificaciones;
+    }
     
-    // Setters and getters
+    
 }
