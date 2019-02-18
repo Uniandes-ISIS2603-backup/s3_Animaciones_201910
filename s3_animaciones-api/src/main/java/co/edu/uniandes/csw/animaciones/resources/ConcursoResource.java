bb352844cf7506f95c88613a08785a6472f86dd0
dfperezc <df.perezc@uniandes.edu.co>
2019-02-17 20:35:00 -0500

    concursoEntity-persistence-test

--- s3_animaciones-api/src/main/java/co/edu/uniandes/csw/animaciones/resources/ConcursoResource.java
+++ s3_animaciones-api/src/main/java/co/edu/uniandes/csw/animaciones/resources/ConcursoResource.java
@@ -7,7 +7,6 @@ import java.util.ArrayList;
 import javax.enterprise.context.RequestScoped;
 import javax.ws.rs.*;
 import java.util.Date;
-import java.util.logging.Logger;
 
 /**
  * @author df.perezc
@@ -18,31 +17,16 @@ import java.util.logging.Logger;
 @RequestScoped
 public class ConcursoResource {
 
-    private final static Logger LOGGER = Logger.getLogger(ConcursoResource.class.getName());
-    /**
-     * crea y retorna un concurso
-     * @param concurso
-     * @return 
-     */
     @POST
     public ConcursoDTO crearConcurso(ConcursoDTO concurso) {
         return concurso;
     }
-    
-    /**
-     * retorna todos los concursos
-     * @return 
-     */
+
     @GET
     public ArrayList<ConcursoDTO> getConcursos() {
         return null;
     }
-    
-    /**
-     * devuelve el concurso de la id que entra por parametro
-     * @param artistaId
-     * @return 
-     */
+
     @GET
     @Path("(concursoId: \\d+)")
     public ConcursoDTO getConcurso(@PathParam("artistaId") Long artistaId) {
@@ -67,25 +51,14 @@ public class ConcursoResource {
      *
      * }
      */
-    /**
-     * crea una nueva regla para el concurso
-     * @param concursoId
-     * @param reglaConcurso
-     * @return 
-     */
     @POST
-    @Path("(concursoId: \\d+)/reglas/(reglasId: \\d+)")
+    @Path("(concursoId: \\d+)")
     public String createReglaConcurso(@PathParam("concursoId") Long concursoId, String reglaConcurso) {
         return reglaConcurso;
     }
 
-    /**
-     * retorna todas las reglas de un concurso
-     * @param concursoId
-     * @return 
-     */
     @GET
-    @Path("(concursoId: \\d+)/reglas")
+    @Path("(concursoId: \\d+)")
     public String getReglasConcurso(@PathParam("concursoId") Long concursoId) {
         return "reglas";
     }
@@ -93,48 +66,25 @@ public class ConcursoResource {
     /**
      * las reglas estan en un arreglo por lo cual para acceder a ellas no se
      * sales de concurso, el identificador de una regla sera su pocision en el 
-     * arreglo.suposición..
-     * @param concursoId
-     * @param reglaId 
+     * arreglo
      */
     @DELETE
-    @Path("(concursoId: \\d+)/reglas/(reglaId)")
+    @Path("(concursoId: \\d+)//")
     public void deleteRegla(@PathParam("concursoId") Long concursoId, @PathParam("reglaId") Long reglaId) {
         
     }
     
-    /**
-     * 
-     * @param concursoId
-     * @param rondaId
-     * @param inicio
-     * @param limite
-     * @param fin
-     * @return 
-     *
     @POST
     @Path("(concursoId: \\d+)/rondas/(rondaId: \\d+)")
     public Date createFechasRonda(@PathParam("concursoId") Long concursoId, @PathParam("rondaId") Long rondaId, Date inicio, Date limite, Date fin) {
         return inicio;
     }
-    */
 
-    
-    /**
-     * 
-     * @param concursoId
-     * @param rondaId
-     * @param inicio
-     * @param limite
-     * @param fin
-     * @return 
-     *
     @PUT
-    @Path("(concursoId: \\d+)/rondas/(rondaId: \\d+)/fechas/(fechaId)")
+    @Path("(concursoId: \\d+)/rondas/(rondaId: \\d+)")
     public Date updateFechasRonda(@PathParam("concursoId") Long concursoId, @PathParam("rondaId") Long rondaId, Date inicio, Date limite, Date fin) {
         return inicio;
     }
-    */
     
     /**
      * solo se usa este metodo en las animaciones finalistas(luego de hacer upgradeRank), solo se requiere poner dinero con un upgrade de animacionConcuso a animacionGanadora
@@ -145,7 +95,7 @@ public class ConcursoResource {
      * @return 
      */
     @POST
-    @Path("(concursoId: \\d+)/rondas/(ronda: \\d+)/ganadores/(animacionGanadoraId: \\+d)")
+    @Path("(concursoId: \\d+)/rondas/(ronda: \\d+)/animacionesConcurso/(animacionConcursoId: \\+d)")
     public Long premiarAnimacion(@PathParam("concursoId") Long concursoId, @PathParam("rondaId") Long rondaId, @PathParam("animacionId") Long animacionConcursoId, Integer dinero)
     {
         return animacionConcursoId;
@@ -155,7 +105,6 @@ public class ConcursoResource {
      * primero se puntua la animacion con este metodo, luego se reankea automaticamente. 
      * @param concursoId
      * @param animacionConcursoId
-     * @param rondaId 
      * @param puntaje
      * @return 
      */
@@ -166,55 +115,26 @@ public class ConcursoResource {
         return animacionConcursoId;
     }
     
-    /**
-     * otorga puesto a la animacion, puede hacerla finalista
-     * @param concursoId
-     * @param rondaId
-     * @param animacionConcursoId
-     * @param rank
-     * @param finalista
-     * @return 
-     *
     @PUT
-    @Path("(concursoId: \\d+)/rondas/(ronda: \\d+)/ganadores")
+    @Path("(concursoId: \\d+)/rondas/(ronda: \\d+)/animacionesConcurso/(animacionConcursoId: \\+d)")
     public Long updateRankAnimacion(@PathParam("concursoId") Long concursoId, @PathParam("rondaId") Long rondaId,@PathParam("animacionId") Long animacionConcursoId, Integer rank, boolean finalista)
     {
         return animacionConcursoId;
     }
-    */
     
-    /**
-     * 
-     * @param concursoId
-     * @param rondaId
-     * @param animacionConcursoId
-     * @return 
-     */
     @GET 
-    @Path("(concursoId: \\d+)/rondas/(ronda: \\d+)/ganadores/(animacionFinalistaId: \\+d)")
+    @Path("(concursoId: \\d+)/rondas/(ronda: \\d+)/animacionesConcurso/(animacionConcursoId: \\+d)")
     public AnimacionConcursoDTO getAnimacion(@PathParam("concursoId") Long concursoId,@PathParam("rondaId") Long rondaId, @PathParam("animacionId") Long animacionConcursoId){
         return null;
     }
     
-    /**
-     * 
-     * @param concursoId
-     * @param animacionId
-     * @return 
-     */
     @POST
-    @Path("(concursoId: \\d+)/animaciones/(animacionID: \\d+)")
+    @Path("concursoId: \\d+")
     public AnimacionConcursoDTO inscribirAnimacion(@PathParam("concursoId") Long concursoId, @PathParam("animacionId") Long animacionId)
     {
       return null;   
     }
     
-    /**
-     * 
-     * @param concursoId
-     * @param rondaId
-     * @param animacionConcursoId 
-     */
     @DELETE
     @Path("(concursoId: \\d+)/rondas/(ronda: \\d+)/animacionesConcurso/(animacionConcursoId: \\+d)")
     public void descalificarAnimacion(@PathParam("concursoId") Long concursoId,@PathParam("rondaId") Long rondaId, @PathParam("animacionId") Long animacionConcursoId)
@@ -229,14 +149,6 @@ public class ConcursoResource {
         return null;
     }
     */
-    /**
-     * 
-     * @param concursoId
-     * @param juradoId
-     * @param ronda
-     * @param votacionId
-     * @return 
-     */
     @GET
     @Path("(concursoId: \\d+)/jurados/(juradoId: \\d+)/votaciones/(votacionesId: \\+d)")
     public VotacionDTO getVotacion(@PathParam("concursoId") Long concursoId,@PathParam("juradoId") Long juradoId, @QueryParam("ronda") Integer ronda, @PathParam("votacionId") Long votacionId)
@@ -244,4 +156,7 @@ public class ConcursoResource {
         return null;
     }   
     
-}
+
+
+           
+            }
